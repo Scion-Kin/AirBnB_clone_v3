72628d3e3a357ec72df3820630b2a151e735ef86
Scion-Kin <dinturner17@gmail.com>
2024-03-31 21:46:20 +0200

    Task 3 done, all checks succeed. Now task 4 attempted.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,25 @@
 from api.v1.views import app_views
 from flask import Flask, Blueprint
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
+    ''' return a status code of OK '''
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('stats')
+def objects():
+    ''' retrieves the number of objects by type '''
+
+    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+
+    len_all = {}
+
+    for i in classes:
+
+        len_all[i] = storage.count(i)
+
+    return jsonify(len_all)
