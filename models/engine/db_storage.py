9f7aea9c0ee5fc97dd516701c2b7198b42a79e6a
Scion-Kin <dinturner17@gmail.com>
2024-03-31 11:04:13 +0200

    Task 1 done!

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,31 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Returns the object based on the class and its ID, or None if not found """
+
+        cls_list = self.all(cls)
+
+        key_id = cls.__name__ + '.' + id
+
+        for i in cls_list:
+
+            if i == key_id:
+                return cls_list[i]
+
+        return None
+
+    def count(self, cls=None):
+        """ Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage. """
+
+        if cls:
+            items = self.all(cls)
+
+            return len(items)
+
+        else:
+            items = self.all()
+
+            return len(items)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,31 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ Returns the object based on the class and its ID, or None if not found """
+
+        cls_list = self.all(cls)
+
+        key_id = cls.__name__ + '.' + id
+
+        for i in cls_list:
+
+            if i == key_id:
+                return cls_list[i]
+
+        return None
+
+    def count(self, cls=None):
+        """ Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage. """
+
+        if cls:
+            items = self.all(cls)
+
+            return len(items)
+
+        else:
+            items = self.all()
+
+            return len(items)
