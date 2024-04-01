05633ef5b61bf871a407a45a4baf040ce09ed5d5
Scion-Kin <dinturner17@gmail.com>
2024-04-01 21:00:02 +0200

    Task 6 corrected

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' The index of the blueprint '''
 from api.v1.views import app_views
-from flask import Flask, Blueprint
+# from flask import Flask, Blueprint
 from flask import jsonify, request, abort, make_response
 from models import storage
 from models.state import State
