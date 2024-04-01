faff5128e2f8d442ffe961fbfdf6151eb1dc6a8c
Scion-Kin <dinturner17@gmail.com>
2024-04-01 19:33:44 +0200

    Task 6 attempted.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,12 +2,13 @@
 ''' The index of the blueprint '''
 from api.v1.views import app_views
 from flask import Flask, Blueprint
-from flask import jsonify, request, abort
+from flask import jsonify, request, abort, make_response
 from models import storage
-import json
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET', 'DELETE', 'POST', 'PUT'])
+@app_views.route('/states/', methods=['GET', 'DELETE', 'POST', 'PUT'])
 @app_views.route('/states/<id>', methods=['GET', 'DELETE', 'POST', 'PUT'])
 def state(id=None):
     ''' The route the handles the state objects '''
@@ -21,9 +22,9 @@ def state(id=None):
             return jsonify(got)
 
         else:
-            state = [state.to_dict() for state in all.values() if state.id == id]
+            state = ([state.to_dict() for state in all.values() if state.id
+                     == id])
             return jsonify(state[0] if len(state) > 0 else {})
-
 
     elif request.method == 'DELETE':
         state = storage.get("State", id) if id else abort(404)
@@ -33,3 +34,35 @@ def state(id=None):
             return jsonify({})
 
         abort(404)
+
+    elif request.method == 'POST':
+        if not request.get_json():
+            return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+        else:
+            if "name" in request.get_json():
+                new = State(**request.get_json())
+                new.save()
+                return new.to_dict()
+
+            else:
+                return make_response(jsonify({"error": "Missing name"}), 400)
+
+
+    else:
+        if not request.get_json():
+            return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+        if not id:
+            abort(404)
+
+        all = storage.all("State")
+        got = [state for state in all.values() if state.id == id]
+
+        state = got[0]
+        for key, value in request.get_json().items():
+            if key not in ['created_at', 'updated_at', 'id']:
+                setattr(state, key, value) 
+        state.save()
+
+        return (state.to_dict())
